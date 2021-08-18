8a95c9a9c1c61bc3c8ff7b2db29b20add2581960
mindolink <ice.cvicek@gmail.com>
2021-08-18 16:52:11 +0200

    Update

--- NodeGethComande/FN1.bash
+++
@@ -1,5 +0,0 @@
-
-
-geth --datadir "/home/blazo/EthNetEIMV/PoA5s" --syncmode "full" --networkid 1994 --http --port 50502 --http.addr "localhost" --http.corsdomain "https://remix.ethereum.org" --http.port 8545 --nat "any" --http.api "web3,eth,net,personal,miner,debug,txpool,admin" --bootnodes "enode://6fc40cc679ca8cbafe1045e1c106c15e86db463e6e89f985ccb7103bfb1fdafe8cbd94daaca5e3a6934d437fcb60ab07d6aa374ee9496308e3f8d321cd3604ef@10.191.110.91:31313" --keystore "/home/blazo/EthDevEIMV/HashNodeKey" --password "/home/blazo/EthDevEIMV/HashNodeKey/password.sec" --allow-insecure-unlock --vmdebug --mine --miner.etherbase 0 --unlock 0
-
-
